1ec5a3ff52d5b71ae1d89b23be71a4fa51b90211
Balaji-Ramadoss <balajiramadoss.r@gmail.com>
2023-11-06 13:43:12 +0530

    my second balajiramadoss.r@gmail.com

--- my-testing/src/App.js
+++ my-testing/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>My React App</h1>
+        <h1>new second row</h1>
       </header>
     </div>
   );
